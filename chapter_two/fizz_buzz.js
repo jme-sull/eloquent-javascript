dfc432b2d780bced90ae5d5451cc4dae824eeca0
Jamie Sullivan <jkcrawshaw@gmail.com>
2021-04-05 12:45:22 -0400

    better solution to fizzbuzz

--- chapter_two/fizz_buzz.js
+++ chapter_two/fizz_buzz.js
@@ -1,12 +1,7 @@
 for(let count = 1; count <= 100; count++){
-    if(count % 3 == 0 && count % 5 == 0){
-        console.log('FizzBuzz')
-    }
-    else if(count % 3 == 0){
-        console.log('Fizz')
-    }else if(count % 5 == 0){
-        console.log('Buzz')
-    }else{
-        console.log(count)
-    }
+    let output = '';
+    if(count % 3 == 0) output += 'Fizz';
+    if(count % 5 == 0) output += 'Buzz';
+    console.log(output || count);
+  
 }
